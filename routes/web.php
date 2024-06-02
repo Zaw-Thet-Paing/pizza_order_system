993d76ef07915c79b5cf08c864312bb90d71a890
Zaw-Thet-Paing <zawthetpaing333@gmail.com>
2024-06-02 11:19:12 +0630

    Login, Logout, Register Process

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,20 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+
+// login, register
+Route::redirect('/', 'loginPage');
+Route::get('loginPage', [AuthController::class, 'loginPage'])->name('auth#loginPage');
+Route::get('registerPage', [AuthController::class, 'registerPage'])->name('auth#registerPage');
+
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified',])->group(function () {
+    //admin
+    //category
+    Route::group(['prefix'=> 'category'], function(){
+        Route::get('list', [CategoryController::class, 'list'])->name('category#list');
+    });
 });
 
-// for pizza order system
-
-Route::get('/', function(){
-    return view('login');
-});
-
-Route::get('/register', function(){
-    return view('register');
-});
