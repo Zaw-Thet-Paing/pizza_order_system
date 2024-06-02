42ffe1c4fe23ecf04cbc3a148ca51a607c6bc5ce
Zaw-Thet-Paing <zawthetpaing333@gmail.com>
2024-06-02 22:09:05 +0630

    Category List Page Design

--- resources/views/admin/category/list.blade.php
+++ resources/views/admin/category/list.blade.php
@@ -1,19 +1,63 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    hello i am admin category page
+@extends('admin.layouts.master')
 
-    <h3>Role - {{ Auth::user()->role }}</h3>
+@section('title', 'Category List')
 
-    <form action="{{ route('logout') }}" method="POST">
-        @csrf
-        <input type="submit" value="Logout">
-    </form>
-</body>
-</html>
+@section('content')
+<div class="section__content section__content--p30">
+    <div class="container-fluid">
+        <div class="col-md-12">
+            <!-- DATA TABLE -->
+            <div class="table-data__tool">
+                <div class="table-data__tool-left">
+                    <div class="overview-wrap">
+                        <h2 class="title-1">Category List</h2>
+
+                    </div>
+                </div>
+                <div class="table-data__tool-right">
+                    <a href="category.html">
+                        <button class="au-btn au-btn-icon au-btn--green au-btn--small">
+                            <i class="zmdi zmdi-plus"></i>Add Category
+                        </button>
+                    </a>
+                    <button class="au-btn au-btn-icon au-btn--green au-btn--small">
+                        CSV download
+                    </button>
+                </div>
+            </div>
+            <div class="table-responsive table-responsive-data2">
+                <table class="table table-data2">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Category Name</th>
+                            <th>Created Date</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="tr-shadow">
+                            <td>1</td>
+                            <td>Samsung S8 Black</td>
+                            <td>2018-09-27 02:12</td>
+                            <td>
+                                CRUD
+                            </td>
+                        </tr>
+                        <tr class="spacer"></tr>
+                        <tr class="tr-shadow">
+                            <td>2</td>
+                            <td>Samsung S8 Black</td>
+                            <td>2018-09-27 02:12</td>
+                            <td>
+                                CRUD
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <!-- END DATA TABLE -->
+        </div>
+    </div>
+</div>
+@endsection
