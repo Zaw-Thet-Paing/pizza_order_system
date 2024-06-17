4f6b415c589d00b31c1721ac2643987b41771c3e
Zaw-Thet-Paing <zawthetpaing333@gmail.com>
2024-06-17 22:33:10 +0630

    Cart Management 1

--- resources/views/user/main/cart.blade.php
+++ resources/views/user/main/cart.blade.php
@@ -17,6 +17,7 @@
                     <tbody class="align-middle">
                         @foreach ($cartList as $cart)
                             <tr>
+                                <input type="hidden" value="{{ $cart->product_price }}" id="pizzaPrice">
                                 <td class="align-middle"><img src="img/product-1.jpg" alt="" style="width: 50px;">{{ $cart->product_name }}</td>
                                 <td class="align-middle">{{ $cart->product_price }} Kyats</td>
                                 <td class="align-middle">
@@ -26,7 +27,7 @@
                                             <i class="fa fa-minus"></i>
                                             </button>
                                         </div>
-                                        <input type="text" class="form-control form-control-sm border-0 text-center" value="{{ $cart->qty }}">
+                                        <input type="text" class="form-control form-control-sm border-0 text-center" value="{{ $cart->qty }}" id="qty">
                                         <div class="input-group-btn">
                                             <button class="btn btn-sm btn-warning btn-plus">
                                                 <i class="fa fa-plus"></i>
@@ -34,15 +35,15 @@
                                         </div>
                                     </div>
                                 </td>
-                                <td class="align-middle">{{ $cart->product_price * $cart->qty }} Kyats</td>
-                                <td class="align-middle"><button class="btn btn-sm btn-danger"><i class="fa fa-times"></i></button></td>
+                                <td class="align-middle" id="total">{{ $cart->product_price * $cart->qty }} Kyats</td>
+                                <td class="align-middle"><button class="btn btn-sm btn-danger btnRemove"><i class="fa fa-times"></i></button></td>
                             </tr>
                         @endforeach
                     </tbody>
                 </table>
             </div>
             <div class="col-lg-4">
-                <h5 class="section-title position-relative text-uppercase mb-3"><span class="bg-secondary pr-3">Cart Summary</span></h5>
+                <h5 class="section-title position-relative text-uppercase mb-3"><span class="pr-3">Cart Summary</span></h5>
                 <div class="bg-light p-30 mb-5">
                     <div class="border-bottom pb-2">
                         <div class="d-flex justify-content-between mb-3">
@@ -59,7 +60,7 @@
                             <h5>Total</h5>
                             <h5>{{ $totalPrice + 3000 }} Kyats</h5>
                         </div>
-                        <button class="btn btn-block btn-primary font-weight-bold my-3 py-3">Proceed To Checkout</button>
+                        <button class="btn btn-block btn-warning font-weight-bold my-3 py-3">Proceed To Checkout</button>
                     </div>
                 </div>
             </div>
@@ -67,4 +68,35 @@
     </div>
     <!-- Cart End -->
 
+@endsection
+
+@section('scriptSource')
+<script>
+$(document).ready(function(){
+    $('.btn-plus').click(function(){
+        $parentNode = $(this).closest("tr");
+        $price = $parentNode.find('#pizzaPrice').val();
+        $qty = parseInt($parentNode.find('#qty').val());
+
+        $total = $price * $qty;
+
+        $parentNode.find('#total').html($total +" Kyats")
+    })
+
+    $('.btn-minus').click(function(){
+        $parentNode = $(this).closest("tr");
+        $price = $parentNode.find('#pizzaPrice').val();
+        $qty = parseInt($parentNode.find('#qty').val());
+
+        $total = $price * $qty;
+
+        $parentNode.find('#total').html($total +" Kyats")
+    })
+
+    $('.btnRemove').click(function(){
+        $parentNode = $(this).closest("tr");
+        $parentNode.remove();
+    })
+})
+</script>
 @endsection
